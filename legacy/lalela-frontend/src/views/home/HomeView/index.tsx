0a329bf4f7e11860f51686ef654b6a65f390ad92
imranparuk <imranparuk@live.com>
2021-05-27 20:17:13 +0200

    update

--- legacy/lalela-frontend/src/views/home/HomeView/index.tsx
+++ legacy/lalela-frontend/src/views/home/HomeView/index.tsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import type { FC } from 'react';
 import { makeStyles } from '@material-ui/core';
 import Page from 'src/components/Page';
-import data from './data.js';
-import schema from './schema.json';
-import uischema from './uischema.json'
+import Hero from './Hero';
+import Features from './Features';
+import Testimonials from './Testimonials';
+import CTA from './CTA';
+import FAQS from './FAQS';
+
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
@@ -13,7 +16,8 @@ import {
   materialRenderers,
   materialCells,
 } from '@jsonforms/material-renderers';
-const initialData = data;
+import { person } from '@jsonforms/examples';
+const initialData = person.data;
 const useStyles = makeStyles(() => ({
   root: {
     display:'flex', justifyContent:'center'
@@ -22,6 +26,186 @@ const useStyles = makeStyles(() => ({
     marginTop: 50
   },
 }));
+
+const scheme = {
+
+  "type": "object",
+  "properties": {
+    "WitnessOrVictim": {
+      "type": "string",
+      "enum": [
+        "Victim",
+        "Witness"
+      ]
+    },
+    "victimAwareness": {
+      "type": "string",
+      "enum": [
+        "Yes",
+        "No"
+      ]
+    },
+    "WitnessFlow": {
+      "type": "object",
+      "properties": {
+        "WitnessWhen": {
+          "type": "object",
+          "properties": {
+            "DateRecall": {
+              "type": "string",
+              "enum": [
+                "Yes",
+                "No"
+              ]
+            },
+            "IncidentDate":{
+              "type": "string",
+              "format": "date"
+            }
+          }
+        },
+        "WitnessWhere": {
+          "type": "object",
+          "properties": {
+            "Where": {
+              "type": "string"
+            }
+          }
+        }
+      }
+    }
+  },
+  "required": [
+    "WitnessOrVictim"
+  ]
+}
+
+const uischeme = {
+  "type": "Categorization",
+  "elements": [
+    {
+      "type": "Category",
+      "label": "Basic Intro",
+      "elements": [
+        {
+          "type": "Group",
+          "label": "Before we begin please let us know if you are reporting an incident agaisnt yourself, or on behalf of another person",
+          "elements": [
+            {
+              "type": "VerticalLayout",
+              "elements": [
+                {
+                  "type": "Control",
+                  "scope": "#/properties/WitnessOrVictim",
+                  "label": "Please indicate if you are reporting on behalf of someone",
+                  "options": {
+                    "format": "radio"
+                  }
+                },
+                {
+                  "type": "Control",
+                  "label": "Does the victim know you are filing this report?",
+                  "scope": "#/properties/victimAwareness",
+                  "options": {
+                    "format": "radio"
+                  },
+                  "rule": {
+                    "effect": "SHOW",
+                    "condition": {
+                      "scope": "#/properties/WitnessOrVictim",
+                      "schema": {
+                        "const": "Witness"
+                      }
+
+                    }
+                  }
+                }
+              ]
+            }
+          ]
+        },
+
+      ]},
+    {
+      "type": "Category",
+      "label": "Time, Date & Location",
+      "elements": [
+        {
+          "type": "VerticalLayout",
+          "elements": [
+            {"type": "Label",
+              "text": "Thank you for taking the time to to report what you have witnessed. This section will focus on the 'where' and 'when' of the incident LOOOONGER SENNNNTENCE"},
+
+            {
+              "type": "HorizontalLayout",
+              "elements": [
+                {"type": "Control",
+                  "label": "Can you recall the Date of the incident?",
+                  "scope": "#/properties/WitnessFlow/properties/WitnessWhen/properties/DateRecall",
+                  "options": {
+                    "format": "radio"
+                  }
+                },
+                {"type": "Control",
+                  "label": "Please indicate the date",
+                  "scope": "#/properties/WitnessFlow/properties/WitnessWhen/properties/IncidentDate",
+                  "rule": {
+                    "effect": "SHOW",
+                    "condition": {
+                      "scope": "#/properties/WitnessFlow/properties/WitnessWhen/properties/DateRecall",
+                      "schema": {
+                        "const": "Yes"
+                      }
+
+                    }
+                  }}]},
+
+
+
+            {"type":"Control",
+              "label": "Where did this incident take place? Any and all information could be imprtant, please give as much detail as possible",
+              "scope": "#/properties/WitnessFlow/properties/WitnessWhere/properties/Where" }
+          ]
+        }
+
+      ],
+      "rule":{
+        "effect":"SHOW",
+        "condition":{
+          "scope": "#/properties/WitnessFlow/properties/WitnessWhen/properties/DateRecall",
+          "schema": {
+            "enum": [
+              "Yes",
+              "No"
+            ]
+          }
+
+        }
+      }
+    },
+    {
+      "type": "Category",
+      "label": "Parties involved",
+      "rule":{
+        "effect":"SHOW",
+        "condition":{
+          "scope": "#/properties/WitnessOrVictim",
+          "schema": {
+            "enum": [
+              "Victim",
+              "Witness"
+            ]
+          }
+
+        }
+      }}
+
+  ],
+  "options": {
+    "variant": "stepper",
+    "showNavButtons": true
+  }
+}
 
 const HomeView: FC = () => {
   const classes = useStyles();
@@ -34,8 +218,8 @@ const HomeView: FC = () => {
       <Card className={classes.card}>
         <CardContent>
           <JsonForms
-            schema={schema}
-            uischema={uischema}
+            schema={scheme}
+            uischema={uischeme}
             data={data}
             renderers={materialRenderers}
             cells={materialCells}
