45df62a6423f13064979e987f3495787de640d2c
imranparuk <imranparuk@live.com>
2021-05-27 20:44:23 +0200

    updates

--- legacy/lalela-frontend/src/views/home/HomeView/index.tsx
+++ legacy/lalela-frontend/src/views/home/HomeView/index.tsx
@@ -14,107 +14,108 @@ import CardContent from '@material-ui/core/CardContent';
 import { JsonForms } from '@jsonforms/react';
 import {
   materialRenderers,
-  materialCells,
+  materialCells
 } from '@jsonforms/material-renderers';
 import { person } from '@jsonforms/examples';
+
 const initialData = person.data;
 const useStyles = makeStyles(() => ({
   root: {
-    display:'flex', justifyContent:'center'
+    display: 'flex', justifyContent: 'center'
   },
   card: {
     marginTop: 50
-  },
+  }
 }));
 
 const scheme = {
 
-  "type": "object",
-  "properties": {
-    "WitnessOrVictim": {
-      "type": "string",
-      "enum": [
-        "Victim",
-        "Witness"
+  'type': 'object',
+  'properties': {
+    'WitnessOrVictim': {
+      'type': 'string',
+      'enum': [
+        'Victim',
+        'Witness'
       ]
     },
-    "victimAwareness": {
-      "type": "string",
-      "enum": [
-        "Yes",
-        "No"
+    'victimAwareness': {
+      'type': 'string',
+      'enum': [
+        'Yes',
+        'No'
       ]
     },
-    "WitnessFlow": {
-      "type": "object",
-      "properties": {
-        "WitnessWhen": {
-          "type": "object",
-          "properties": {
-            "DateRecall": {
-              "type": "string",
-              "enum": [
-                "Yes",
-                "No"
+    'WitnessFlow': {
+      'type': 'object',
+      'properties': {
+        'WitnessWhen': {
+          'type': 'object',
+          'properties': {
+            'DateRecall': {
+              'type': 'string',
+              'enum': [
+                'Yes',
+                'No'
               ]
             },
-            "IncidentDate":{
-              "type": "string",
-              "format": "date"
+            'IncidentDate': {
+              'type': 'string',
+              'format': 'date'
             }
           }
         },
-        "WitnessWhere": {
-          "type": "object",
-          "properties": {
-            "Where": {
-              "type": "string"
+        'WitnessWhere': {
+          'type': 'object',
+          'properties': {
+            'Where': {
+              'type': 'string'
             }
           }
         }
       }
     }
   },
-  "required": [
-    "WitnessOrVictim"
+  'required': [
+    'WitnessOrVictim'
   ]
-}
+};
 
 const uischeme = {
-  "type": "Categorization",
-  "elements": [
+  'type': 'Categorization',
+  'elements': [
     {
-      "type": "Category",
-      "label": "Basic Intro",
-      "elements": [
+      'type': 'Category',
+      'label': 'Basic Intro',
+      'elements': [
         {
-          "type": "Group",
-          "label": "Before we begin please let us know if you are reporting an incident agaisnt yourself, or on behalf of another person",
-          "elements": [
+          'type': 'Group',
+          'label': 'Before we begin please let us know if you are reporting an incident agaisnt yourself, or on behalf of another person',
+          'elements': [
             {
-              "type": "VerticalLayout",
-              "elements": [
+              'type': 'VerticalLayout',
+              'elements': [
                 {
-                  "type": "Control",
-                  "scope": "#/properties/WitnessOrVictim",
-                  "label": "Please indicate if you are reporting on behalf of someone",
-                  "options": {
-                    "format": "radio"
+                  'type': 'Control',
+                  'scope': '#/properties/WitnessOrVictim',
+                  'label': 'Please indicate if you are reporting on behalf of someone',
+                  'options': {
+                    'format': 'radio'
                   }
                 },
                 {
-                  "type": "Control",
-                  "label": "Does the victim know you are filing this report?",
-                  "scope": "#/properties/victimAwareness",
-                  "options": {
-                    "format": "radio"
+                  'type': 'Control',
+                  'label': 'Does the victim know you are filing this report?',
+                  'scope': '#/properties/victimAwareness',
+                  'options': {
+                    'format': 'radio'
                   },
-                  "rule": {
-                    "effect": "SHOW",
-                    "condition": {
-                      "scope": "#/properties/WitnessOrVictim",
-                      "schema": {
-                        "const": "Witness"
+                  'rule': {
+                    'effect': 'SHOW',
+                    'condition': {
+                      'scope': '#/properties/WitnessOrVictim',
+                      'schema': {
+                        'const': 'Witness'
                       }
 
                     }
@@ -123,60 +124,71 @@ const uischeme = {
               ]
             }
           ]
-        },
-
-      ]},
-    {
-      "type": "Category",
-      "label": "Time, Date & Location",
-      "elements": [
-        {
-          "type": "VerticalLayout",
-          "elements": [
-            {"type": "Label",
-              "text": "Thank you for taking the time to to report what you have witnessed. This section will focus on the 'where' and 'when' of the incident LOOOONGER SENNNNTENCE"},
-
-            {
-              "type": "HorizontalLayout",
-              "elements": [
-                {"type": "Control",
-                  "label": "Can you recall the Date of the incident?",
-                  "scope": "#/properties/WitnessFlow/properties/WitnessWhen/properties/DateRecall",
-                  "options": {
-                    "format": "radio"
-                  }
-                },
-                {"type": "Control",
-                  "label": "Please indicate the date",
-                  "scope": "#/properties/WitnessFlow/properties/WitnessWhen/properties/IncidentDate",
-                  "rule": {
-                    "effect": "SHOW",
-                    "condition": {
-                      "scope": "#/properties/WitnessFlow/properties/WitnessWhen/properties/DateRecall",
-                      "schema": {
-                        "const": "Yes"
-                      }
-
-                    }
-                  }}]},
-
-
-
-            {"type":"Control",
-              "label": "Where did this incident take place? Any and all information could be imprtant, please give as much detail as possible",
-              "scope": "#/properties/WitnessFlow/properties/WitnessWhere/properties/Where" }
-          ]
         }
 
+      ]
+    },
+    {
+      'type': 'Category',
+      'label': 'Time, Date & Location',
+      'elements': [
+        {
+          'type': 'Group',
+          'label': 'Thank you for taking the time to to report what you have witnessed. This section will focus on the \'where\' and \'when\' of the incident. The sentance continues here but not in caps lock',
+          'elements': [
+            {
+              'type': 'VerticalLayout',
+              'elements': [
+                {
+                  'type': 'HorizontalLayout',
+                  'elements': [
+                    {
+                      'type': 'Control',
+                      'label': 'Can you recall the Date of the incident?',
+                      'scope': '#/properties/WitnessFlow/properties/WitnessWhen/properties/DateRecall',
+                      'options': {
+                        'format': 'radio'
+                      }
+                    },
+                    {
+                      'type': 'Control',
+                      'label': 'Incident Date',
+                      'scope': '#/properties/WitnessFlow/properties/WitnessWhen/properties/IncidentDate',
+                      'rule': {
+                        'effect': 'SHOW',
+                        'condition': {
+                          'scope': '#/properties/WitnessFlow/properties/WitnessWhen/properties/DateRecall',
+                          'schema': {
+                            'const': 'Yes'
+                          }
+                        }
+                      }
+                    }
+                  ]
+                },
+                {
+                  'type': 'Label',
+                  'text': 'Where did this incident take place? Any and all information could be imprtant, please give as much detail as possible',
+                },
+                {
+                  'type': 'Control',
+                  'scope': '#/properties/WitnessFlow/properties/WitnessWhere/properties/Where'
+                }
+              ]
+            }
+          ]
+        },
+
+
       ],
-      "rule":{
-        "effect":"SHOW",
-        "condition":{
-          "scope": "#/properties/WitnessFlow/properties/WitnessWhen/properties/DateRecall",
-          "schema": {
-            "enum": [
-              "Yes",
-              "No"
+      'rule': {
+        'effect': 'SHOW',
+        'condition': {
+          'scope': '#/properties/WitnessFlow/properties/WitnessWhen/properties/DateRecall',
+          'schema': {
+            'enum': [
+              'Yes',
+              'No'
             ]
           }
 
@@ -184,28 +196,29 @@ const uischeme = {
       }
     },
     {
-      "type": "Category",
-      "label": "Parties involved",
-      "rule":{
-        "effect":"SHOW",
-        "condition":{
-          "scope": "#/properties/WitnessOrVictim",
-          "schema": {
-            "enum": [
-              "Victim",
-              "Witness"
+      'type': 'Category',
+      'label': 'Parties involved',
+      'rule': {
+        'effect': 'SHOW',
+        'condition': {
+          'scope': '#/properties/WitnessOrVictim',
+          'schema': {
+            'enum': [
+              'Victim',
+              'Witness'
             ]
           }
 
         }
-      }}
+      }
+    }
 
   ],
-  "options": {
-    "variant": "stepper",
-    "showNavButtons": true
+  'options': {
+    'variant': 'stepper',
+    'showNavButtons': true
   }
-}
+};
 
 const HomeView: FC = () => {
   const classes = useStyles();
@@ -213,7 +226,7 @@ const HomeView: FC = () => {
   return (
     <Page
       className={classes.root}
-      title="Home"
+      title='Home'
     >
       <Card className={classes.card}>
         <CardContent>
